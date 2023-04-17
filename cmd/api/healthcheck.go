40c064e0836d14a650557ab239b6975a60a8862a
iamsecb <iamsecb@gmail.com>
2023-04-17 19:45:26 +1000

    refactor(healthcheck): Fix return value format in func doc

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -11,7 +11,7 @@ type healthcheck struct {
 	Version     string `json:"version"`
 }
 
-// Declare a handler which writes a plain-text response with information about the
+// Declare a handler which writes a json response with information about the
 // application status, operating environment and version.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	h := healthcheck{
